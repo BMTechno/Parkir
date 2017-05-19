9af2459dd0b9dedb596c2674bfed9bd9621f48a9
ALBERT ALFRIANTA <albert.alfrianta@gmail.com>
2017-05-19 23:42:30 +0700

    Add new feature Upgrade

--- app/src/main/java/com/tomcat/parkir/MainActivity.java
+++ app/src/main/java/com/tomcat/parkir/MainActivity.java
@@ -27,76 +27,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-    private ProgressDialog pDialog;
-    private DB db;
-    private User user;
-    private Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);     //menghilangkan heading title
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();	//biar koneksi bisa dijalanin di main, karena aturannya koneksi gk boleh di Main langsung
-        StrictMode.setThreadPolicy(policy);
-        User user = new User(this);
-        if(user.getPassword()!=null){
-            Log.d("User_id",user.getPassword());
-            new Auth(user).execute();       //autentikasi user dan password ke server
-        }
-    }
-    class Auth extends AsyncTask<Integer, Integer, Integer> {
-        User user;
-        Auth(User user) {
-            this.user = user;
-        }
-        // ### Before starting background thread Show Progress Dialog ###
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(MainActivity.this);
-            pDialog.setMessage("Login...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
 
-        // ### Login Auth ###
-        protected Integer doInBackground(Integer... args) {
-            Log.d("User_id2",user.getPassword());
-            DB db = new DB(getApplicationContext(),user);
-            int signal=db.auth();
-            if (signal==0){   //jika sukses
-                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(i);
-                finish();
-            }
-            return signal;
-        }
-
-        // ### After completing background task ###
-        protected void onPostExecute(Integer signal) {
-            // dismiss the dialog once done
-            pDialog.dismiss();
-            switch (signal) {
-                case 0:
-                    break;
-                case 1:
-                    Toast.makeText(getApplicationContext(), getString(R.string.mainFailedLogin), Toast.LENGTH_SHORT).show();
-                    break;
-                case 2:
-                    Toast.makeText(getApplicationContext(), getString(R.string.signal4), Toast.LENGTH_SHORT).show();
-                    break;
-                case 3:
-                    Toast.makeText(getApplicationContext(), getString(R.string.signal2), Toast.LENGTH_SHORT).show();
-                    //i = new Intent(getApplicationContext(), Login.class);
-                    //startActivity(i);
-                    //finish();
-                    break;
-                case 4:
-                    Toast.makeText(getApplicationContext(), getString(R.string.signal3), Toast.LENGTH_SHORT).show();
-                    break;
-            }
-        }
     }
 
     public void ClickDaftar(View V){
